dfffdfc796faac17c73a1f88ce0cf3f06cdea3d4
Sam <samuelglerum@hotmail.com>
2020-06-16 12:28:14 +0200

    Added comment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config(); // Load dotenv for using environment variables
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // If set, set PORT to env variable. Otherwise set to 3000
 
 app.get("*", (req, res) => {
     res.send("Welcome to the Coronapp API!");
