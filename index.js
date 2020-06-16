048387c4e25e387ca6565238eaaba6c2f1e825cd
Sam <samuelglerum@hotmail.com>
2020-06-16 12:26:22 +0200

    Created default api response

--- index.js
+++ index.js
@@ -3,14 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-
-
-
-
-
-
-
+app.get("*", (req, res) => {
+    res.send("Welcome to the Coronapp API!");
+});
 
 app.listen(PORT, () => {
     console.log("API is listening on port " + PORT);
