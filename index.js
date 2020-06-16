73b5a7b2f45c638c48936fabc0e901b8cc7045d2
Sam <samuelglerum@hotmail.com>
2020-06-16 14:11:55 +0200

    Created db connection

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 require('dotenv').config(); // Load dotenv for using environment variables
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = process.env.PORT || 3000; // If set, set PORT to env variable. Otherwise set to 3000
+
+try {
+    let CONNECTION_STRING = "mongodb+srv://baseuser:" + process.env.DB_PASS +"@coronapp-c4dvi.mongodb.net/" + process.env.DB_NAME + "?retryWrites=true&w=majority";
+    mongoose.connect(CONNECTION_STRING, {useNewUrlParser: true})
+} catch (error) {
+    console.log(error)
+}
 
 app.get("*", (req, res) => {
     res.send("Welcome to the Coronapp API!");
